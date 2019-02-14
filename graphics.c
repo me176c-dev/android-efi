59bdce85a0965e01900776a83881d7534add61bf
lambdadroid <lambdadroid@gmail.com>
2019-02-14 16:20:56 +0100

    graphics: Simplify code
    Add center() helper method and use it for both x/y.

--- graphics.c
+++ graphics.c
@@ -4,6 +4,10 @@
 #include "graphics.h"
 #include <efilib.h>
 
+static inline UINTN center(UINTN i, UINTN max) {
+    return i < max ? (max - i) / 2 : 0;
+}
+
 EFI_STATUS graphics_display_image(struct graphics_image *image) {
     EFI_GRAPHICS_OUTPUT_PROTOCOL *graphics_output;
     EFI_STATUS err = LibLocateProtocol(&GraphicsOutputProtocol, (VOID**) &graphics_output);
@@ -18,23 +22,8 @@ EFI_STATUS graphics_display_image(struct graphics_image *image) {
     }
 
     // Calculate position of image
-    UINTN hres = graphics_output->Mode->Info->HorizontalResolution;
-    UINTN x;
-    if (image->width < hres) {
-        x = (hres - image->width) / 2;
-    } else {
-        x = 0;
-    }
-
-    UINTN vres = graphics_output->Mode->Info->VerticalResolution;
-
-    UINTN y;
-    if (image->height < vres) {
-        y = (vres - image->height) / 2;
-    } else {
-        y = 0;
-    }
-
+    UINTN x = center(image->width, graphics_output->Mode->Info->HorizontalResolution);
+    UINTN y = center(image->height, graphics_output->Mode->Info->VerticalResolution);
     return uefi_call_wrapper(graphics_output->Blt, 10, graphics_output, image->blt, EfiBltBufferToVideo,
                              0, 0, x, y, image->width, image->height, 0);
 }
